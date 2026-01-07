92685c538ef94c086609ba49be4a6c78773089bb
Al-Fareed <testvagrant@Al-Fa.local>
2026-01-07 17:28:07 +0530

    fixed for special keys

--- e2e_test.py
+++ e2e_test.py
@@ -1,32 +1,35 @@
-from time import time
+import time
 import os
 from core.context import DriverContext
 from dotenv import load_dotenv
-import time
 
 load_dotenv()
 
 RTSP_URL = os.getenv("RTSP_URL")
 
-def test_search():
+def end_to_end_test():
     ctx = DriverContext(RTSP_URL)
     ctx.start()
     try:
-        # ctx.goto("For You","App Settings")
-        # ctx.press("ENTER")
-        # time.sleep(5)
-        # isExpectedScreenDisplayed =  ctx.assert_screen("Am I in Settings(DEVELOPE MODE) page?")
-        # print("Is user viewing app screen",isExpectedScreenDisplayed)
-        # ctx.goto("App Settings","Sony LIV Delete")
-        # ctx.press("ENTER")
+        ctx.goto("For You","App Settings")
+        ctx.press("ENTER")
+        time.sleep(5)
+        isExpectedScreenDisplayed =  ctx.assert_screen("Am I in Settings(DEVELOPE MODE) page?")
+        print("Is user viewing app screen",isExpectedScreenDisplayed)
+        ctx.goto("App Settings","Sony LIV Delete")
+        ctx.press("ENTER")
+        ctx.press("LEFT")
+        ctx.press("ENTER")
 
-        # ctx.press("HOME")
-        # ctx.press("HOME")
-        # isUserOnHomeScreen = ctx.assert_screen("Am I in ```For You``` tab?")
-        # print("Is user on Home Screen",isUserOnHomeScreen)
+        ctx.press("HOME")
+        isUserOnHomeScreen = ctx.assert_screen("Am I in ```For You``` tab?")
+        print("Is user on Home Screen",isUserOnHomeScreen)
         ctx.goto("For You","Search")
         ctx.press("ENTER")
         time.sleep(5)
+        isExpectedScreenDisplayed = ctx.assert_screen("Am I in Search screen?")
+        print("Is user viewing search screen",isExpectedScreenDisplayed)
+
         ctx.goto("Search","Search Input")
         ctx.press("ENTER")
         isExpectedScreenDisplayed =  ctx.assert_screen("Is Virtual Keyboard Visible?")
@@ -40,4 +43,4 @@ def test_search():
         ctx.shutdown()
         
 if __name__ == "__main__":
-    test_search()
+    end_to_end_test()
--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,24 @@
+import os
 from datetime import datetime
+from threading import Lock
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_NAME = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log")
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
+
+_lock = Lock()
 
 def log_event(step_id: int, message: str) -> None:
-    ts = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-    print(f"[{ts}] [STEP {step_id}] {message}")
+    """
+    Logs an event to both console and a timestamped log file.
+    """
+    timestamp = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    log_line = f"[{timestamp}] [STEP {step_id}] {message}"
+
+    with _lock:
+        print(log_line)
+
+        with open(LOG_FILE_PATH, "a", encoding="utf-8") as f:
+            f.write(log_line + "\n")
