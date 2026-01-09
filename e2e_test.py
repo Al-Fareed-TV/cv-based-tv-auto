3076d178695d4295f65d835d64c135bb387ea359
Ayush Sinha <ayushs@testvagrant.com>
2026-01-09 16:23:59 +0530

    Added Sony LIV Testcase

--- e2e_test.py
+++ e2e_test.py
@@ -18,7 +18,7 @@ def end_to_end_test():
         ctx.goto("For You","Sony LIV")
         ctx.press("ENTER")
         ctx.longWait(15)
-        isSonyLivScreenDisplayed = ctx.assert_screen("Am I in Sony LIV screen?")
+        isSonyLivScreenDisplayed = ctx.assert_screen("Sony LIV Home Screen",sys_prompt["sony_liv_home"])
         print("Is user viewing sony liv screen",isSonyLivScreenDisplayed)
         ctx.longWait(15)
         ctx.press("DOWN")
@@ -29,17 +29,30 @@ def end_to_end_test():
         ctx.longWait(5)
         ctx.press("ENTER")
         ctx.longWait(5)
-        isSonyLivScreenDisplayed = ctx.assert_screen("Am I in Movies screen?")
+        isSonyLivScreenDisplayed = ctx.assert_screen("Sony LIV Movies Screen", sys_prompt["sony_liv_movies"])
         print("Is user viewing movies screen",isSonyLivScreenDisplayed)
+        ctx.longWait(10)
+        ctx.goto("Sony LIV Movies","Movie")
+        ctx.longWait(5)
         ctx.press("ENTER")
         ctx.longWait(10)
         ctx.press("ENTER")
         ctx.longWait(10)
-        isSonyLivScreenDisplayed = ctx.assert_screen("Settings is shown on screen ?")
+        isSonyLivScreenDisplayed = ctx.assert_screen("Sony LIV Video Player", sys_prompt["sony_liv_video_player"])
         print("Is player is shown on screen",isSonyLivScreenDisplayed)
         ctx.press("RIGHT")
         ctx.longWait()
         ctx.press("LEFT")
+
+        #Plan Check
+        ctx.press("RETURN")
+        ctx.longWait()
+        ctx.press("UP")
+        ctx.longWait(10)
+        ctx.press("ENTER")
+        ctx.longWait()
+        isSonyLivScreenDisplayed = ctx.assert_screen("Sony LIV Subscription Screen", sys_prompt["sony_liv_subscription"])
+        print("Is user viewing subscription screen",isSonyLivScreenDisplayed)
 
         # Uninstall the Sony LIV App
         ctx.press("HOME")
@@ -48,7 +61,7 @@ def end_to_end_test():
         ctx.press("ENTER")
         ctx.longWait()
         isExpectedScreenDisplayed = ctx.assert_screen(
-            "Settings(DEVELOPE MODE)",
+            "Settings(DEVELOP MODE)",
             sys_prompt["settings_develop_mode"]
         )
         print("Is user viewing app screen", isExpectedScreenDisplayed)
